4b37d268435b316501ea62b08a8b862b8a9957ca
Pablo Orviz <orviz@ifca.unican.es>
2020-09-29 10:31:31 +0200

    Prototyping a DB store using shelve for adding pipelines

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -1,8 +1,24 @@
+import shelve
+import uuid
+
 from typing import List, Dict
 from aiohttp import web
 
 from openapi_server.models.pipeline import Pipeline
 from openapi_server import util
+
+
+def load_db_content():
+    return shelve.open('sqaaas')
+
+
+def store_db_content(d):
+    with shelve.open('db.shelve') as db:
+        db = d
+        print('### Pipeline DB ##')
+        for k in db.keys():
+            print(k, db[k])
+        print('##################')
 
 
 async def add_pipeline(request: web.Request, body) -> web.Response:
@@ -14,7 +30,13 @@ async def add_pipeline(request: web.Request, body) -> web.Response:
     :type body: dict | bytes
 
     """
+    pipeline_id = str(uuid.uuid4())
     body = Pipeline.from_dict(body)
+    d = {'id': body.pipeline_id, 'sqa_criteria': body.sqa_criteria}
+    db = load_db_content()
+    db[pipeline_id] = d
+    store_db_content(db)
+
     return web.Response(status=200)
 
 
