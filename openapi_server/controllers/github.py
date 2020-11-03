8816e74e82a5f9e788adeab86f7be07500f28af4
Pablo Orviz <orviz@ifca.unican.es>
2020-11-03 18:16:55 +0100

    Fix in class

--- openapi_server/controllers/github.py
+++ openapi_server/controllers/github.py
@@ -5,22 +5,21 @@ class GitHubUtils(object):
     """Class for handling requests to GitHub API.
 
     Support only for token-based access.
-
     """
-    def __init__(self, access_token=None):
+    def __init__(self, access_token):
         """GitHubUtils object definition.
 
         :param access_token: GitHub's access token
         """
-        self.client = GitHub(self.access_token)
+        self.client = Github(access_token)
 
-    def get_org_repository(repo_name, org_name='eosc-synergy'):
+    def get_org_repository(self, repo_name, org_name='eosc-synergy'):
         org = self.client.get_organization(org_name)
         try:
             return org.get_repo(repo_name.raw_data)
         except github.GithubException.UnknownObjectException:
             return False
 
-    def create_org_repository(repo_name, org_name='eosc-synergy'):
+    def create_org_repository(self, repo_name, org_name='eosc-synergy'):
         org = self.client.get_organization(org_name)
         repo = org.create_repo(repo_name)
