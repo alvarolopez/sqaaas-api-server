f310a619861bc2a19757d2334d91a9e590e40fcf
Pablo Orviz <orviz@ifca.unican.es>
2020-12-04 14:27:22 +0100

    Enable debug through CLI

--- openapi_server/__init__.py
+++ openapi_server/__init__.py
@@ -6,9 +6,9 @@ import logging
 from openapi_server import config
 
 
-def set_log():
+def set_log(debug=False):
     logger = logging.getLogger('sqaaas_api')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
     ch = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
@@ -35,6 +35,11 @@ def set_parser():
         dest='port',
         default='8080',
         help='Port number to be used when exposing the API server')
+    parser.add_argument(
+        '-d',
+        '--debug',
+        action="store_true",
+        help='Set DEBUG log level')
 
     return parser.parse_args()
 
@@ -45,7 +50,7 @@ def main():
         "swagger_ui": True
     }
 
-    set_log()
+    set_log(options_cli.debug)
     config.init(options_cli.config_file)
 
     specification_dir = os.path.join(os.path.dirname(__file__), 'openapi')
