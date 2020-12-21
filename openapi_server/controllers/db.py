113bca542096bc45fb816faa0163d77f520d658d
Pablo Orviz <orviz@ifca.unican.es>
2020-12-21 09:07:48 +0100

    Use pathlib instead of os.path

--- openapi_server/controllers/db.py
+++ openapi_server/controllers/db.py
@@ -1,25 +1,31 @@
 import json
 import logging
-import os
+import pathlib
 
 from openapi_server import config
 
 
-DB_FILE = config.get('db_file', fallback='/sqaaas/sqaaas.json')
+DB_FILE = pathlib.Path(
+    config.get('db_file', fallback='/sqaaas/sqaaas.json'))
 logger = logging.getLogger('sqaaas_api.controller.db')
 
 
 def load_content():
     data = {}
-    if os.path.exists(DB_FILE) and os.stat(DB_FILE).st_size > 0:
-        with open(DB_FILE) as db:
-            data = json.load(db)
+    if DB_FILE.exists():
+        data = json.loads(DB_FILE.read_text(encoding='utf-8'))
     return data
 
 
 def store_content(data):
-    with open(DB_FILE, 'w') as db:
-        json.dump(data, db)
+    try:
+        DB_FILE.parent.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        logger.debug('DB file path: parent folder already exists')
+    else:
+        logger.debug('DB file path: parent folder created')
+
+    DB_FILE.write_text(json.dumps(data), encoding='utf-8')
     print_content()
 
 
