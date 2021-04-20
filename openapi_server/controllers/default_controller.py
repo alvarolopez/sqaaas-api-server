f64fbfc3c862f1e101f5b4dcf8b0bd80fc928d1d
Pablo Orviz <orviz@ifca.unican.es>
2021-04-20 11:48:06 +0200

    Setup Badgr config

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -25,17 +25,30 @@ JENKINS_URL = config.get_ci('url')
 JENKINS_USER = config.get_ci('user')
 JENKINS_GITHUB_ORG = config.get_ci('github_organization_name')
 
+TOKEN_BADGR_FILE = config.get_badge(
+    'token', fallback='/etc/sqaaas/.badgr_token')
+BADGR_URL = config.get_badge('url')
+BADGR_USER = config.get_badge('user')
+
 logger = logging.getLogger('sqaaas_api.controller')
 
+# Instance of code repo backend object
 with open(TOKEN_GH_FILE,'r') as f:
     token = f.read().strip()
 logger.debug('Loading GitHub token from local filesystem')
 gh_utils = GitHubUtils(token)
 
+# Instance of CI system object
 with open(TOKEN_JK_FILE,'r') as f:
     jk_token = f.read().strip()
 logger.debug('Loading Jenkins token from local filesystem')
 jk_utils = JenkinsUtils(JENKINS_URL, JENKINS_USER, jk_token)
+
+# Instance of Badge issuing service object
+with open(TOKEN_BADGR_FILE,'r') as f:
+    badgr_token = f.read().strip()
+logger.debug('Loading Badgr password from local filesystem')
+badgr_utils = BadgrUtils(BADGR_URL, BADGR_USER, badgr_token)
 
 
 @ctls_utils.extended_data_validation
