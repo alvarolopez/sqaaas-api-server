5d790c0f1c3d1163ab4ed551218a1a703933cada
Pablo Orviz <orviz@ifca.unican.es>
2021-05-13 11:29:15 +0200

    Separate JePL file name generator from append_file_name() method

--- openapi_server/controllers/jepl.py
+++ openapi_server/controllers/jepl.py
@@ -7,28 +7,42 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 class JePLUtils(object):
     """Class that generates JePL configuration files."""
     @staticmethod
-    def append_file_name(file_type, file_data_list):
+    def generate_file_name(file_type, random=False):
+        """Generates a file name for any of the JePL-related types of file
+
+        :param file_type: Type of JePL file, one of [config, composer, jenkinsfile].
+        :param random: Boolean that marks whether a random string should be inserted in the file name.
+        """
+        file_type_chunks = {
+            'config': ['.sqa/config', 'yml'],
+            'composer': ['.sqa/docker-compose', 'yml'],
+            'jenkinsfile': ['Jenkinsfile'],
+            'commands_script': ['.sqa/script', 'sh']
+        }
+        chunk_list = copy.deepcopy(file_type_chunks[file_type])
+        random_str = namegenerator.gen()
+        chunk_list.insert(1, random_str)
+
+        return '.'.join(chunk_list)
+
+    @staticmethod
+    def append_file_name(file_type, file_data_list, force_random_name=False):
         """Appends a 'file_name' property, according to its type (file_type),
         to each Dict element of the given List (file_data_list)
 
         :param file_type: Type of JePL file, one of [config, composer, jenkinsfile].
         :param file_data_list: List of JSON payload data to be associated with the generated file name.
+        :param force_random_name: If set the method will always return a random name for the file.
         """
-        file_type_chunks = {
-            'config': ['.sqa/config', 'yml'],
-            'composer': ['.sqa/docker-compose', 'yml'],
-            'jenkinsfile': ['Jenkinsfile']
-        }
-
         new_file_data_list = []
         count = 0
         for data in file_data_list:
-            chunk_list = copy.deepcopy(file_type_chunks[file_type])
-            if count > 0:
-                random_str = namegenerator.gen()
-                chunk_list.insert(1, random_str)
+            if count > 0 or force_random_name:
+                file_name = generate_file_name(file_type, random=True)
+            else:
+                file_name = generate_file_name(file_type)
             new_data = copy.deepcopy(data)
-            new_data.update({'file_name': '.'.join(chunk_list)})
+            new_data.update({'file_name': file_name})
             new_file_data_list.append(new_data)
             count += 1
 
